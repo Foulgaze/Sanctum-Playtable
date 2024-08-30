f6e34fabf675e702cba148d1eb80337da3c676c8
Gabe Righi <gaberighi@gmail.com>
2024-08-30 08:52:30 -0500

    Began adding drag

--- Assets/DragController.cs
+++ Assets/DragController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -11,32 +12,44 @@ public class DragController : MonoBehaviour
     private Transform? currentDraggedObject;
     private GraphicRaycaster raycaster;
     private PointerEventData pointerEventData;
+    [SerializeField] private EventSystem eventSystem;
     private Vector2 offset;
+    private int draggableMask;
 
     void Start()
     {
         raycaster = GetComponent<GraphicRaycaster>();
+        pointerEventData = new(eventSystem);
+        draggableMask = LayerMask.NameToLayer("Draggable");
     }
     public bool IsDragging()
     {
-        return currentDraggedObject == null;
+        return currentDraggedObject != null;
     }
 
     private Transform? RaycastForUI()
     {
         List<RaycastResult> results = new List<RaycastResult>();
+        pointerEventData = new PointerEventData(eventSystem);
+        pointerEventData.position = Input.mousePosition;
         raycaster.Raycast(pointerEventData, results);
-        results = results.OrderBy(result => result.distance).ToList();
-        results = results.OrderBy(result => result.sortingOrder).ToList();
-        if(results.Count() == 0)
+        if(results.Count == 0)
         {
             return null;
         }
-        return results[0].gameObject.transform;
-
+        
+        results = results.OrderBy(result => result.distance).ThenByDescending(result => result.sortingOrder).ToList();
+        RaycastResult hit = results[0];
+        Debug.Log($"{hit.gameObject.name}");
+        if(hit.gameObject.layer != draggableMask)
+        {
+            return null;
+        }
+        
+        return hit.gameObject.transform;
     }
 
-    void Update()
+    private void CheckForStartDrag()
     {
         if(Input.GetMouseButtonDown((int)MouseButton.Left))
         {
@@ -46,16 +59,32 @@ public class DragController : MonoBehaviour
                 return;
             }
             currentDraggedObject = hit;
-        }
-        if(IsDragging())
-        {
-            UpdateDraggedCard();
+            offset = hit.position - Input.mousePosition;
         }
     }
-
-    private void UpdateDraggedCard()
+    private void CheckForReleaseDrag()
     {
-        currentDraggedObject.transform.position = Input.mousePosition + offset;
+        if(!IsDragging() || Input.GetMouseButton((int) MouseButton.Left))
+        {
+            return;
+        }
+        currentDraggedObject = null;
+    }
+
+    void Update()
+    {
+        CheckForStartDrag();
+        CheckForReleaseDrag();
+        UpdateDragPosition();
+    }
+
+    private void UpdateDragPosition()
+    {
+        if(!IsDragging())
+        {
+            return;
+        }
+        currentDraggedObject.position = (Vector2)Input.mousePosition + offset;
     }
 
 }
